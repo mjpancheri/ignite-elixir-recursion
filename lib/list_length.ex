12b0aaabe39f94edca52ab8fc54195a579718147
Marcio Pancheri <mjpancheri@gmail.com>
2022-01-30 17:19:49 -0300

    add count list function

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: listCount(list, 0)
+
+  defp listCount([], acc), do: acc
+
+  defp listCount([_], acc), do: acc + 1
+
+  defp listCount([_head | tail], acc) do
+    listCount(tail, acc + 1)
   end
 end
